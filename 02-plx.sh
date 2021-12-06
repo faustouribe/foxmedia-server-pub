be8c030bd33cdb18bddefea58e9a6a7d5ec0eba3
faustouribe <faustouribe@gmail.com>
2021-12-05 21:17:26 -0600

    Config files directory

--- 02-plx.sh
+++ 02-plx.sh
@@ -5,7 +5,7 @@ docker create \
   -e PGID=1000 \
   -e VERSION=docker \
   -e UMASK_SET=022 \
-  -v /server/plex/config/:/config \
-  -v /server/plex/transcode/:/transcode \
+  -v /home/mrfox/plex/config/:/config \
+  -v /home/mrfox/plex/transcode/:/transcode \
   -v /library/media/:/library \
   ghcr.io/linuxserver/plex:latest
