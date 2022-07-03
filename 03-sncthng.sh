4b7a5069a9723b9a0e5ce1780ddfe1b4e0301b99
faustouribe <faustouribe@gmail.com>
2022-07-02 20:17:27 -0500

    Use variables

--- 03-sncthng.sh
+++ 03-sncthng.sh
@@ -2,12 +2,12 @@ docker create \
   --name=syncthing \
   -e PUID=1000 \
   -e PGID=1000 \
-  -e TZ=America/Chicago \
+  -e TZ=$TIMEZONE \
   -e UMASK_SET=022 \
   -p 8384:8384 \
   -p 22000:22000 \
   -p 21027:21027/udp \
-  -v /home/mrfox/syncthing/:/config \
-  -v /library/media/:/library \
+  -v $PATHTOCONFIG/syncthing/:/config \
+  -v $PATHTOLIBRARY/:/library \
   --restart unless-stopped \
   linuxserver/syncthing
