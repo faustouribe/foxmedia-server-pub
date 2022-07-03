b6ad0205b99da1f3cba46529074970326b1a3e9c
faustouribe <faustouribe@gmail.com>
2022-07-02 20:40:20 -0500

    Use variables

--- 07-wrgrd.sh
+++ 07-wrgrd.sh
@@ -1,22 +1,23 @@
+#!/bin/bash
 # Para ver QRcode
 # docker exec -it wireguard /app/show-peer 1
-
+source local \
 docker run -d \
   --name=wireguard \
   --cap-add=NET_ADMIN \
   --cap-add=SYS_MODULE \
   -e PUID=1000 \
   -e PGID=1000 \
-  -e TZ=America/Chicago \
-  -e SERVERURL=xxxxxx.duckdns.org \
+  -e TZ=$TIMEZONE \
+  -e SERVERURL=$DCKDNSSUBDOMAIN.duckdns.org \
   -e SERVERPORT=51820 \
   -e PEERS=1 \
   -e PEERDNS=auto \
   -e INTERNAL_SUBNET=10.13.13.0 \
   -e ALLOWEDIPS=10.13.13.0/24,192.168.1.0/24,0.0.0.0/0 \
   -p 51820:51820/udp \
-  -v /home/mrfox/wireguard:/config \
+  -v $PATHTOCONFIG/wireguard:/config \
   -v /lib/modules:/lib/modules \
   --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
   --restart unless-stopped \
-  ghcr.io/linuxserver/wireguard
+ghcr.io/linuxserver/wireguard
