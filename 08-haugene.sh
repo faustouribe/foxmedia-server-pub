d7e7297fef95d6ce5919992ab25e8e7afd9f13d4
faustouribe <faustouribe@gmail.com>
2022-07-02 22:45:24 -0500

    Use variables

--- 08-haugene.sh
+++ 08-haugene.sh
@@ -6,12 +6,13 @@ docker run --cap-add=NET_ADMIN -d \
   -v /etc/localtime:/etc/localtime:ro \
   -e CREATE_TUN_DEVICE=true \
   -e OPENVPN_PROVIDER=EXPRESSVPN \
-  -e OPENVPN_CONFIG=my_expressvpn_usa_-_dallas_udp \
-  --env-file ./env.haugene \
+  -e OPENVPN_CONFIG=$OVPNCNF \
+  -e OPENVPN_USERNAME=$OVPNUSR \
+  -e OPENVPN_PASSWORD=$OVPNPWD \
   -e WEBPROXY_ENABLED=true \
   -e WEBPROXY_PORT=8888 \
   -e LOCAL_NETWORK=192.168.1.0/24 \
-  -e TRANSMISSION_DOWNLOAD_DIR=/data/inter/movies \
+  -e TRANSMISSION_DOWNLOAD_DIR=/data/inter/downloads \
   -e OPENVPN_OPTS="--inactive 3600 --ping 10 --ping-exit 60" \
   --log-driver json-file \
   --log-opt max-size=10m \
