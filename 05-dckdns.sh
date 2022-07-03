60e425f03ec607d3605b10dc43e13e19afc2af9a
faustouribe <faustouribe@gmail.com>
2022-07-02 20:32:55 -0500

    Use variables

--- 05-dckdns.sh
+++ 05-dckdns.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+source local \
 docker run -d \
   --name=duckdns \
   -e PUID=1000 \
   -e PGID=1000 \
-  -e TZ=America/Chicago \
-  -e SUBDOMAINS=subdomain1,subdomain2 \
-  -e TOKEN=token \
+  -e TZ=$TIMEZONE \
+  -e SUBDOMAINS=$DCKDNSSUBDOMAINS \
+  -e TOKEN=$DCKDNSTOKEN \
   -e LOG_FILE=true  \
-  -v /home/mrfox/duckdns:/config \
+  -v $PATHTOCONFIG/duckdns:/config \
   --restart unless-stopped \
   linuxserver/duckdns
