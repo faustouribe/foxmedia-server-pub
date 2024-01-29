062be9b352b0ea600d95ae9147119766e10923e9
faustouribe <faustouribe@gmail.com>
2024-01-29 15:09:49 -0600

    Update autoremove.sh

--- autoremove.sh
+++ autoremove.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-transmission-remote --auth $tmpUser:$tmpPass -l | awk '$2 == "100%" && (( $9 == "Stopped" || $9 == "Finished" )){ system("transmission-remote --auth $tmpUser:$tmpPass -t " $1 " --remove") }'
+echo "cron ran @: $(date)" >> /data/transmission-home/autoremove.log
+transmission-remote --auth $tmpUser:$tmpPass -l | awk '$2 == "100%" && (( $9 == "Stopped" || $9 == "Finished" )){ system("transmission-remote --auth $tmpUser:$tmpPass -t " $1 " --remove") }' >> /data/transmission-home/autoremove.log
