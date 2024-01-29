aeee60e9230fef5370b555457b959b1d18e7e97a
faustouribe <faustouribe@gmail.com>
2024-01-29 17:11:32 -0600

    Update autoremove.sh

--- autoremove.sh
+++ autoremove.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sleep 10
+sleep 30
 echo "settings-torrent-done -> ran @: $(date)" >> /data/transmission-home/autoremove.log
 transmission-remote --auth $tmpUser:$tmpPass -l | awk '{print $2,$9,$10}' >> /data/transmission-home/autoremove.log
-transmission-remote --auth $tmpUser:$tmpPass -l | awk '$2 == "100%" && (( $9 == "Stopped" || $9 == "Finished" )){ system("transmission-remote --auth $tmpUser:$tmpPass -t " $1 " --remove") }' >> /data/transmission-home/autoremove.log
+transmission-remote --auth $tmpUser:$tmpPass -l | awk '$2 == "100%" && (( $9 == "Stopped" || $9 == "Finished" || $9 == "Seeding" )){ system("transmission-remote --auth $tmpUser:$tmpPass -t " $1 " --remove") }' >> /data/transmission-home/autoremove.log
+~
