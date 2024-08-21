3498be747ce2e2068b0067b4f17ccdc00932f66a
faustouribe <faustouribe@gmail.com>
2024-08-21 18:32:41 -0500

    Updated library path

--- 09-jellyfin.sh
+++ 09-jellyfin.sh
@@ -11,11 +11,6 @@ docker run -d \
           -p 7359:7359/udp \
           -p 1900:1900/udp \
           -v $PATHTOCONFIG/jellyfin:/config \
-          -v $PATHTOLIBRARY/tv:/data/tv \
-          -v $PATHTOLIBRARY/f-tv:/data/f-tv \
-          -v $PATHTOLIBRARY/inter/tv:/data/inter-tv \
-          -v $PATHTOLIBRARY/movies:/data/movies \
-          -v $PATHTOLIBRARY/f-movies:/data/f-movies \
-          -v $PATHTOLIBRARY/inter/movies:/data/inter-movies \
+          -v $PATHTOLIBRARY:/data \
           $RESTART \
 lscr.io/linuxserver/jellyfin:latest
